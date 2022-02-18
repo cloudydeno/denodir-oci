a1e117ce8cb7f3720ceecdc9d00ef079f27536f1
Daniel Lamando <dan@danopia.net>
2022-02-18 21:47:53 +0100

    refactor: Split out docker config manipulation

--- lib/eject-to-image.ts
+++ lib/eject-to-image.ts
@@ -13,10 +13,12 @@ import * as OciStore from "./store.ts";
 import { Sha256Writer } from "./util/digest.ts";
 import { gunzipReaderToWriter } from "./util/gzip.ts";
 import { stableJsonStringify } from "./util/serialize.ts";
+import { ImageConfigWriter } from "./util/image-config.ts";
 
 /**
- * combine a base docker image with a denodir artifact
- * results in a runnable image
+ * Combine a base docker image with a denodir artifact
+ * Results in a runnable image
+ * Tested with: Docker, podman, containerd
  */
 export async function ejectToImage(opts: {
   store: OciStore.Api;
@@ -25,8 +27,7 @@ export async function ejectToImage(opts: {
   annotations?: Record<string, string>;
 }) {
 
-  const rawBaseMani = await opts.store.getFullLayer('manifest', opts.baseDigest);
-  const baseManifest: Manifest = JSON.parse(new TextDecoder().decode(rawBaseMani));
+  const baseManifest: Manifest = JSON.parse(new TextDecoder().decode(await opts.store.getFullLayer('manifest', opts.baseDigest)));
 
   // When given a multi-arch manifest, produce a multi-arch manifest as well
   if (baseManifest.mediaType == MEDIATYPE_OCI_MANIFEST_INDEX_V1
@@ -53,22 +54,16 @@ export async function ejectToImage(opts: {
     throw new Error(`Base manifest at ${opts.baseDigest} has unsupported mediaType`);
   }
 
-  const dociManifestRaw = await opts.store.getFullLayer('manifest', opts.dociDigest);
-  const dociManifest: ManifestOCI = JSON.parse(new TextDecoder().decode(dociManifestRaw));
+  const dociManifest: ManifestOCI = JSON.parse(new TextDecoder().decode(await opts.store.getFullLayer('manifest', opts.dociDigest)));
 
-  const baseConfig: OciImageConfig = JSON.parse(new TextDecoder().decode(await opts.store.getFullLayer('blob', baseManifest.config.digest)));
+  const baseConfiig: OciImageConfig = JSON.parse(new TextDecoder().decode(await opts.store.getFullLayer('blob', baseManifest.config.digest)));
   const dociConfig: DenodirArtifactConfig = JSON.parse(new TextDecoder().decode(await opts.store.getFullLayer('blob', dociManifest.config.digest)));
 
-  const knownDenodir = baseConfig.config.Env.find(x => x.startsWith('DENO_DIR='));
-  if (knownDenodir !== 'DENO_DIR=/denodir') {
-    baseConfig.config.Env = baseConfig.config.Env.filter(x => !x.startsWith('DENO_DIR='));
-    baseConfig.config.Env.push(`DENO_DIR=/denodir`);
-    baseConfig.history?.push({
-      empty_layer: true,
-      created: new Date().toISOString(),
-      created_by: "ENV DENO_DIR=/denodir",
-      comment: `cloudydeno.denodir-oci.v0`,
-    });
+  const configWriter = new ImageConfigWriter(baseConfiig, `cloudydeno.denodir-oci.v0`);
+
+  const knownDenodir = configWriter.getEnv('DENO_DIR');
+  if (knownDenodir !== '/denodir') {
+    configWriter.setEnv(`DENO_DIR`, `/denodir`);
   }
 
   // Add the DOCI layers to the Docker config
@@ -78,41 +73,23 @@ export async function ejectToImage(opts: {
       await opts.store.getLayerReader('blob', layer.digest),
       uncompressedHasher);
 
-    const uncompressedSha256 = uncompressedHasher.toHexString();
-
-    baseConfig.history?.push({
-      created: new Date().toISOString(),
-      created_by: `RUN deno cache ${layer.annotations?.['specifier'].replace('file:///denodir/deps/file/', '') ?? '[...]'}`,
-      comment: `cloudydeno.denodir-oci.v0`,
+    configWriter.recordDiffLayer({
+      command: `RUN deno cache ${layer.annotations?.['specifier'].replace('file:///denodir/deps/file/', '') ?? '[...]'}`,
+      diffDigest: `sha256:${uncompressedHasher.toHexString()}`,
     });
-    baseConfig.rootfs.diff_ids.push(`sha256:${uncompressedSha256}`);
   }
 
-  baseConfig.config.Entrypoint = [
+  configWriter.setEntrypoint([
     `deno`, `run`,
     `--cached-only`,
     ...dociConfig.runtimeFlags,
     dociConfig.entrypoint,
-  ];
-  baseConfig.config.Cmd = [];
-  baseConfig.history?.push({
-    empty_layer: true,
-    created: new Date().toISOString(),
-    created_by: `ENTRYPOINT [${baseConfig.config.Cmd.map(x => JSON.stringify(x)).join(' ')}]`,
-    comment: `cloudydeno.denodir-oci.v0`,
-  });
-  baseConfig.history?.push({
-    empty_layer: true,
-    created: new Date().toISOString(),
-    created_by: `CMD []`,
-    comment: `cloudydeno.denodir-oci.v0`,
-  });
-
-  baseConfig.created = new Date().toISOString();
+  ]);
+  configWriter.setCommand([]);
 
   const configDesc = await opts.store.putLayerFromString('blob', {
     mediaType: "application/vnd.oci.image.config.v1+json",
-  }, stableJsonStringify(baseConfig));
+  }, stableJsonStringify(configWriter.data));
 
   const manifestDesc = await opts.store.putLayerFromString('manifest', {
     mediaType: MEDIATYPE_OCI_MANIFEST_V1,
