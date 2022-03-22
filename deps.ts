5f33198f03c6e08457e4645e677642292ab95ff8
Daniel Lamando <dan@danopia.net>
2022-03-22 13:49:06 +0100

    Update dependencies: /std@0.130.0

--- deps.ts
+++ deps.ts
@@ -2,15 +2,15 @@ export {
   readAll, copy, writeAll,
   readableStreamFromReader,
   readerFromIterable,
-} from "https://deno.land/std@0.120.0/streams/conversion.ts";
-export { Tar, Untar } from "https://deno.land/std@0.120.0/archive/tar.ts";
-export { assert, assertEquals } from "https://deno.land/std@0.120.0/testing/asserts.ts";
-export * as path from "https://deno.land/std@0.120.0/path/mod.ts";
-export { Buffer } from "https://deno.land/std@0.120.0/io/buffer.ts";
-export { Sha256 } from "https://deno.land/std@0.120.0/hash/sha256.ts";
-export { parse as parseYaml } from "https://deno.land/std@0.120.0/encoding/yaml.ts";
+} from "https://deno.land/std@0.130.0/streams/conversion.ts";
+export { Tar, Untar } from "https://deno.land/std@0.130.0/archive/tar.ts";
+export { assert, assertEquals } from "https://deno.land/std@0.130.0/testing/asserts.ts";
+export * as path from "https://deno.land/std@0.130.0/path/mod.ts";
+export { Buffer } from "https://deno.land/std@0.130.0/io/buffer.ts";
+export { Sha256 } from "https://deno.land/std@0.130.0/hash/sha256.ts";
+export { parse as parseYaml } from "https://deno.land/std@0.130.0/encoding/yaml.ts";
 
-export type { ModuleGraphJson } from "https://deno.land/x/deno_graph@0.18.0/lib/types.d.ts";
+export type { ModuleGraphJson } from "https://deno.land/x/deno_graph@0.25.0/lib/types.d.ts";
 
 export type {
   Manifest,
@@ -18,7 +18,7 @@ export type {
   ManifestV2, ManifestV2Descriptor, ManifestV2List,
   RegistryImage,
   RegistryClientOpts,
-} from "https://deno.land/x/docker_registry_client@v0.3.3/index.ts";
+} from "https://deno.land/x/docker_registry_client@v0.3.4/index.ts";
 export {
   RegistryClientV2,
   RegistryHttpError,
@@ -27,12 +27,12 @@ export {
   MEDIATYPE_OCI_MANIFEST_INDEX_V1,
   MEDIATYPE_MANIFEST_V2,
   MEDIATYPE_MANIFEST_LIST_V2,
-} from "https://deno.land/x/docker_registry_client@v0.3.3/index.ts";
+} from "https://deno.land/x/docker_registry_client@v0.3.4/index.ts";
 
-export { komando, defineCommand } from "https://deno.land/x/komando@v1.0.1/mod.js";
-export type { Flags, Args } from "https://deno.land/x/komando@v1.0.1/mod.js";
+export { komando, defineCommand } from "https://deno.land/x/komando@v1.0.2/mod.js";
+export type { Flags, Args } from "https://deno.land/x/komando@v1.0.2/mod.js";
 
 export { forEach } from "https://deno.land/x/stream_observables@v1.2/transforms/for-each.ts";
 
-import ProgressBar from "https://deno.land/x/progress@v1.2.4/mod.ts";
+import ProgressBar from "https://deno.land/x/progress@v1.2.5/mod.ts";
 export { ProgressBar };
